7af34b4e7ff99a2c6cf6726a3d5d3e5436254d93
richardson-david <richardsond@newpaltz.edu>
2025-04-20 00:13:09 -0400

    Small updates to the NLDAS download file.

--- 01_Scripts/03_ThinIce-GLM-NLDS-download.R
+++ 01_Scripts/03_ThinIce-GLM-NLDS-download.R
@@ -101,7 +101,7 @@ out.ts = seq.POSIXt(as.POSIXct(startdatetime, tz = loc_tz),as.POSIXct(enddatetim
 
   #In case any need rerunning, can create a specific list here. SOmetimes, it appears that the download doesn;t work. You can tell this if the nc file size is 2KB instead of 90KB 
   #out.ts<-c(as.POSIXct("2019-01-11 07:00:00", tz = loc_tz),as.POSIXct("2019-01-12 01:00:00", tz = loc_tz),as.POSIXct("2019-06-24 03:00:00", tz = loc_tz),as.POSIXct("2019-11-19 21:00:00", tz = loc_tz))
-  #out.ts<-c(as.POSIXct("2021-05-29 01:00:00", tz = loc_tz),as.POSIXct("2021-10-15 02:00:00", tz = loc_tz))
+  #out.ts<-c(as.POSIXct("2022-06-22 15:00:00", tz = loc_tz),as.POSIXct("2022-07-19 17:00:00", tz = loc_tz))
 
 # Create output list of tables
 output = list()
