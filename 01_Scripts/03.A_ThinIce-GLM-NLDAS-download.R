1d7c54f7d63ae5591d56ab9151b5b731413d3ef1
richardson-david <richardsond@newpaltz.edu>
2025-04-30 07:16:30 -0400

    Adding back in 2024 for Bethel

--- 01_Scripts/03.A_ThinIce-GLM-NLDAS-download.R
+++ 01_Scripts/03.A_ThinIce-GLM-NLDAS-download.R
@@ -57,7 +57,7 @@ source("01_Scripts/00_ThinIce-GLM-Functions.R")
 ###########################################################
 lakeNumber<-"08" #number of the folder####
 lakeName<-"Bethel" #Lake name here####
-year<-2021 #set the year here, GL4: 2018-2024; 2018-2020: KG: B, 2020-2021 IAO: C; 2022-2024 DCR: A#### 
+year<-2024 #set the year here, GL4: 2018-2024; 2018-2020: KG: B, 2020-2021 IAO: C; 2022-2024 DCR: A#### 
 local_tz_set = "US/Central" #enter the timezone you would like to have the final data in. see OlsonNames() for options, e.g., "EST", "US/Mountain", "US/Central" ####
 loc_tz = 'GMT' #only run in tz's without DST, otherwise you will be very sad when you go to collate and it's a mess.####
 extent = as.numeric(c(-92.34,38.90,-92.33,38.91)) #Enter in the decimal degree bounding box of your lake rounded to two decimals####
