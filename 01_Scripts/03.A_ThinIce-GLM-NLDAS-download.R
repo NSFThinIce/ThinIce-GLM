06c2ec6479e87534f58e7674b3960ddc48bc79b6
richardson-david <richardsond@newpaltz.edu>
2025-05-03 22:18:30 -0400

    Starting in on Marceline - 2024

--- 01_Scripts/03.A_ThinIce-GLM-NLDAS-download.R
+++ 01_Scripts/03.A_ThinIce-GLM-NLDAS-download.R
@@ -55,12 +55,12 @@ source("01_Scripts/00_ThinIce-GLM-Functions.R")
 ###########################################################
 ### Step 1. LAKE DATA ####
 ###########################################################
-lakeNumber<-"08" #number of the folder####
-lakeName<-"Bethel" #Lake name here####
-year<-2019 #set the year here, GL4: 2018-2024; 2018-2020: KG: B, 2020-2021 IAO: C; 2022-2024 DCR: A#### 
+lakeNumber<-"09" #number of the folder####
+lakeName<-"Marceline" #Lake name here####
+year<-2024 #set the year here, GL4: 2018-2024; 2018-2020: KG: B, 2020-2021 IAO: C; 2022-2024 DCR: A#### 
 local_tz_set = "US/Central" #enter the timezone you would like to have the final data in. see OlsonNames() for options, e.g., "EST", "US/Mountain", "US/Central" ####
 loc_tz = 'GMT' #only run in tz's without DST, otherwise you will be very sad when you go to collate and it's a mess.####
-extent = as.numeric(c(-92.34,38.90,-92.33,38.91)) #Enter in the decimal degree bounding box of your lake rounded to two decimals####
+extent = as.numeric(c(-93.00,39.66,-92.98,39.68)) #Enter in the decimal degree bounding box of your lake rounded to two decimals####
 #if the extent is loaded from the shapefile (above), make sure they are in decimal degrees, otherwise this code will not work
 ##########################################################
 
@@ -516,7 +516,7 @@ drivers <- drivers %>%
          WindSpeed = WindSpeed.m_s,
          Rain = Rain.m_day,
          Snow = Snow.m_day ) %>% 
-  select(-c(SurfPressure.Pa, SpecHumidity.kg_kg))
+  dplyr::select(-c(SurfPressure.Pa, SpecHumidity.kg_kg))
 
 #Plot a few of the drivers####
 plot(drivers$time,drivers$Rain,type = 'l')
