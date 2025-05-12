2d9e69f939ecf22d607a42b945e8b543a286a1d0
Your Name <kgannon59401@gmail.com>
2025-05-12 12:04:06 -0600

    Gl4 2019

--- 01_Scripts/03.B_ThinIce-GLM-NLDAS-download.R
+++ 01_Scripts/03.B_ThinIce-GLM-NLDAS-download.R
@@ -132,7 +132,7 @@ out.ts = seq.POSIXt(as.POSIXct(startdatetime, tz = loc_tz),as.POSIXct(enddatetim
   ##############################
   #In case any need rerunning, can create a specific list here. SOmetimes, it appears that the download doesn;t work. You can tell this if the nc file size is 2KB instead of 90KB 
   #Do not uncomment these - but just run the code after the #
-  #out.ts<-c(as.POSIXct("2019-01-11 07:00:00", tz = loc_tz),as.POSIXct("2019-01-12 01:00:00", tz = loc_tz),as.POSIXct("2019-06-24 03:00:00", tz = loc_tz),as.POSIXct("2019-11-19 21:00:00", tz = loc_tz))
+  #out.ts<-c(as.POSIXct("2019-12-31 23:00:00", tz = loc_tz),as.POSIXct("2019-12-31 15:00:00", tz = loc_tz),as.POSIXct("2019-12-31 11:00:00", tz = loc_tz),as.POSIXct("2019-12-17 11:00:00", tz = loc_tz))
   #out.ts<-c(as.POSIXct("2024-10-20 07:00:00", tz = loc_tz),as.POSIXct("2024-11-15 23:00:00", tz = loc_tz))
 
 # Create output list of tables
